e8d7434e4a9c344fa1f8d34b50d06f06d0494602
kairimoriyama <kairimoriyama@LAPTOP-Q3PT6T9R.localdomain>
2021-04-18 07:05:39 +0900

    update 0418

--- mmproject/settings.py
+++ mmproject/settings.py
@@ -92,8 +92,12 @@ AUTH_USER_MODEL = 'accounts.User'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mmproject',
+        'USER': 'postgres',
+        'PASSWORD': 'PASSWORD',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
