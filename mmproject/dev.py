18281d0008ba8f7716bc592b17acc23b4bd15853
kairimoriyama <kairimoriyama@gmail.com>
2021-05-08 16:54:13 +0900

    軽微な修正

--- mmproject/dev.py
+++ mmproject/dev.py
@@ -26,9 +26,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'allauth.socialaccount.providers.google',
 
-    'django.contrib.humanize',
-
     #'django_ses',
+    'django.contrib.humanize',
     'django_resized',
 ]
 
