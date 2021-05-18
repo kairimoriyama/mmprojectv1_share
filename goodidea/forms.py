cb669d49af42c7393bdb015588106e0f4da6655a
kairimoriyama <kairimoriyama@gmail.com>
2021-05-18 18:53:20 +0900

    update

--- goodidea/forms.py
+++ goodidea/forms.py
@@ -41,41 +41,13 @@ class ItemCreateFromIdea(ModelForm):
 
     class Meta:
         model  = Item
-        fields = ('itemNum','ideaNum','submissionDate','progress','division','staff','category','system','purchase','title','description','refURL1','refURL2','refURL3','picture1','picture2','picture3','picture4','picture5','picture6','refFile1','refFile2','refFile3','inchargeDivision','inchargeStaff' )
+        fields = ('submissionDate','progress','division','staff','category','system','purchase','title','description','refURL1','refURL2','refURL3','picture1','picture2','picture3','picture4','picture5','picture6','refFile1','refFile2','refFile3','inchargeDivision','inchargeStaff' )
         widgets = {'submissionDate': DateInput()}
 
     def __init__(self, *args, **kwargs):
         super(ItemCreateFromIdea, self).__init__(*args, **kwargs)
 
-        
-        # ideaNumの設定
-        currentYear= datetime.date.today().year
-        currentYearStr = str(currentYear)
-        currentYearIdeaNums = Item.objects.values('ideaNum').filter(
-            submissionDate__year=currentYear
-            )
-        lastIdeaNum = currentYearIdeaNums.aggregate(Max('ideaNum'))
-        maxIdeaNum = lastIdeaNum['ideaNum__max']
-
-        if (maxIdeaNum == None) or (maxIdeaNum < 1):
-            self.fields['ideaNum'].initial = int(currentYearStr[-2:] + "0001")
-        else:
-            self.fields['ideaNum'].initial = maxIdeaNum +1 
-
-        # itemNumの設定
-        ItemNums = Item.objects.values('itemNum')
-        lastItemNum = ItemNums.aggregate(Max('itemNum'))
-        maxItemNum = lastItemNum['itemNum__max']
-
-        if (maxItemNum == None) or (maxItemNum < 1):
-            self.fields['itemNum'].initial = 1
-        else:
-            self.fields['itemNum'].initial = maxItemNum +1 
-
-
         # 初期値・入力規則
-        self.fields['itemNum'].widget.attrs['readonly'] = 'readonly'
-        self.fields['ideaNum'].widget.attrs['readonly'] = 'readonly'
         self.fields['progress'].initial = 1
         self.fields['staff'].required = True
         self.fields['division'].required = True
@@ -95,43 +67,14 @@ class ItemCreateFromAction(ModelForm):
 
     class Meta:
         model  = Item
-        fields = ('itemNum','actionNum','submissionDate','progress','division','staff','category','system','purchase','title','description','refURL1','refURL2','refURL3','picture1','picture2','picture3','picture4','picture5','picture6','refFile1','refFile2','refFile3','inchargeDivision','inchargeStaff', 'completionDate' )
+        fields = ('submissionDate','progress','division','staff','category','system','purchase','title','description','refURL1','refURL2','refURL3','picture1','picture2','picture3','picture4','picture5','picture6','refFile1','refFile2','refFile3','inchargeDivision','inchargeStaff', 'completionDate' )
         widgets = {'submissionDate': DateInput(), 'completionDate': DateInput()}
     
     def __init__(self, *args, **kwargs):
         super(ItemCreateFromAction, self).__init__(*args, **kwargs)
 
-        # actionNumの設定
-        currentYear= datetime.date.today().year
-        currentYearStr = str(currentYear)
-        currentYearActionNums = Item.objects.values('actionNum').filter(
-            submissionDate__year=currentYear
-            )
-        lastActionNum = currentYearActionNums.aggregate(Max('actionNum'))
-        maxActionNum = lastActionNum['actionNum__max']
-
-        print(maxActionNum)
-
-        if (maxActionNum == None) or (maxActionNum < 1):
-            self.fields['actionNum'].initial = int(currentYearStr[-2:] + "001")
-        else:
-            self.fields['actionNum'].initial = maxActionNum +1 
-
-
-        # itemNumの設定
         
-        ItemNums = Item.objects.values('itemNum')
-        lastItemNum = ItemNums.aggregate(Max('itemNum'))
-        maxItemNum = lastItemNum['itemNum__max']
-
-        if (maxItemNum == None) or (maxItemNum < 1):
-            self.fields['itemNum'].initial = 1
-        else:
-            self.fields['itemNum'].initial = maxItemNum +1 
-
         # 初期値・入力規則
-        self.fields['itemNum'].widget.attrs['readonly'] = 'readonly'
-        self.fields['actionNum'].widget.attrs['readonly'] = 'readonly'
         self.fields['progress'].initial = 4
         self.fields['completionDate'].required = True
         self.fields['staff'].required = True
