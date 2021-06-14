72e5cbc5f46853ded628630c8f0782430d7de297
kairimoriyama <kairimoriyama@gmail.com>
2021-06-14 09:07:35 +0900

    update

--- procurement/views.py
+++ procurement/views.py
@@ -213,12 +213,147 @@ class ListRequest(ListView):
     ).order_by('adminCheck__no','-orderInfo__orderNum')
     paginate_by = 22
 
+    def __init__(self, **kwargs):
+        super(ItemListFilter, self).__init__(**kwargs)
+        self.form = None
     
     def get_context_data(self, **kwargs):
         context = super(ListRequest, self).get_context_data(**kwargs)
-        self.request.session['returnPage_procurement'] = 2
-        print(self.request.session['returnPage_procurement'])
+
+        # 検索結果を保持
+        context.update(dict(form=self.form, query_string=self.request.GET.urlencode()))
+
+        self.request.session['returnPage_procurement'] = 2 # ページ移動
+
+        context['divisionSelect_list'] = Division.objects.all()
+        
+        # StaffQuerySet のstaff_activeで絞込
+        context['staffdb_list'] = StaffDB.objects.staff_active()
+
         return context
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+       
+     
+        # 入力した検索条件の取得
+        staffdb = self.request.GET.get('staffdb')
+        division = self.request.GET.get('division')
+        word = self.request.GET.get('word')
+        submissionDateFrom = self.request.GET.get('submissionDateFrom')
+        submissionDateTo = self.request.GET.get('submissionDateTo')
+        settlementDateFrom = self.request.GET.get('settlementDateFrom')
+        settlementDateTo = self.request.GET.get('settlementDateTo')
+        settlementCheck = self.request.GET.get('settlementCheck')
+
+
+        #  値をセッションで保持
+        
+        self.request.session['staffdb'] = staffdb
+        self.request.session['division'] = division
+        self.request.session['word'] = word
+        self.request.session['submissionDateFrom'] = submissionDateFrom
+        self.request.session['submissionDateTo'] = submissionDateTo
+        self.request.session['settlementDateFrom'] = settlementDateFrom
+        self.request.session['settlementDateTo'] = settlementDateTo
+        self.request.session['settlementCheck'] = settlementCheck
+
+        # 絞り込み前の初期値
+        queryset0 = Item.objects_list.all()
+
+        # ページ遷移直後でなければ値がNullではないため絞込可能
+        if progress or purchase or system or staffdb or division or inchargeStaff or inchargeDivision or word or\
+            (submissionDateFrom and submissionDateTo) or (completionDateFrom and completionDateTo) or internalDiscussion:
+
+            # 協議案件/共有案件
+            if ideaOrAction == "0":   #協議案件のみ
+                queryset1 = queryset0.filter( ideaNum__gt = 0)
+            elif ideaOrAction == "1": #共有案件のみ
+                queryset1 = queryset0.filter( actionNum__gt = 0)
+            else:
+                queryset1 = queryset0.all()
+
+            # 購入案件の絞込
+            if purchase == "1":
+                queryset2 = queryset1.filter(purchase__exact=True)
+            else:
+                queryset2 = queryset1.all()
+
+            # システム案件の絞込
+            if system == "1":
+                queryset3 = queryset2.filter(system__exact=True)
+            else:
+                queryset3 = queryset2.all()
+
+            # 所属の絞り込み
+            if division == "0": #全部門
+                queryset4 = queryset3.all()
+            else:               #全部門以外
+                queryset4 = queryset3.filter(division__exact=division)
+            
+            # 進捗・完了日の絞り込み
+            if progress == "0":   #全進捗
+                queryset5 = queryset4.all()
+            elif progress == "4": #完了案件のみ
+                queryset5 = queryset4.filter(progress__exact=4
+                ).filter(completionDate__range=(completionDateFrom, completionDateTo))
+            else:                 #新規/実施なし
+                queryset5 = queryset4.filter(progress__exact=progress)
+
+            # 日付の絞込
+            if (submissionDateFrom and submissionDateTo) :
+                queryset6 = queryset5.filter(
+                    submissionDate__range=(submissionDateFrom, submissionDateTo)
+                )
+            else:                 
+                queryset6 = queryset5.all()
+
+            # 担当者の絞込
+            if staffdb == "0":   #全社員
+                queryset7 = queryset6.all()
+            else: 
+                queryset7 = queryset6.filter(staffdb__exact=staffdb)
+            
+            # 実行担当者の絞込
+            if inchargeStaff:
+                queryset8 = queryset7.filter(inchargeStaff__icontains=inchargeStaff)
+            else: 
+                queryset8 = queryset7.all()    
+
+            # 実行担当部署の絞込
+            if inchargeDivision:
+                queryset9 = queryset8.filter(inchargeDivision__icontains=inchargeDivision)
+            else: 
+                queryset9 = queryset8.all()   
+
+             # キーワードの絞込
+            if word :
+                queryset10 = queryset9.filter(
+                    Q(title__icontains=word)| Q(description__icontains=word)|
+                    Q(discussionNote__icontains=word)| Q(report__icontains=word))
+            else: 
+                queryset10 = queryset9.all() 
+
+
+            # 部門決裁の絞り込み
+            if internalDiscussion == "1":
+                queryset11 = queryset10.filter(internalDiscussion__exact=True)
+            else:
+                queryset11 = queryset10.all()
+
+            # セッションで選択されたデータを保持
+            self.request.session['item_list_type'] = 'filter'
+            
+            queryset = queryset11.order_by('-itemNum')
+
+        # ページ遷移直後のNullでは絞込なし
+        else:
+            qeryset = queryset0.order_by('-itemNum')
+        
+        return queryset.order_by('-itemNum')
+
+
+
 
 
 class ListOrder(ListView):
@@ -227,6 +362,7 @@ class ListOrder(ListView):
     fields = '__all__'
     queryset = OrderInfo.objects.filter(deletedItem=False)
     paginate_by = 22
+
 
 
 
