bfe7b921c0b60d307a5f7feac25cdcaadbdb0250
kairimoriyama <kairimoriyama@gmail.com>
2021-06-15 13:59:02 +0900

    pudate

--- bankstatement/models.py
+++ bankstatement/models.py
@@ -3,6 +3,29 @@ from django.utils import timezone
 from django.urls import reverse
 
 
+
+class Progress(models.Model):
+    no = models.IntegerField(blank=True,null=True)
+    name = models.CharField(max_length=50)
+    
+    class Meta:
+        ordering = ('no',)
+
+    def __str__(self):
+        return self.name
+
+
+class FinancialCategory(models.Model):
+    no = models.IntegerField(blank=True,null=True)
+    name = models.CharField(max_length=50)
+    
+    class Meta:
+        ordering = ('no',)
+
+    def __str__(self):
+        return self.name
+
+
 class AccountType(models.Model):
     no = models.IntegerField(blank=False,null=False)
     name = models.CharField(max_length=50,blank=False,null=False)
@@ -16,13 +39,25 @@ class AccountType(models.Model):
 
 class BankAccount(models.Model):
     no = models.IntegerField(blank=False,null=False)
-    name = models.CharField(max_length=50,blank=False,null=False)
+    branchName = models.CharField(max_length=50,blank=False,null=False)
+    accountNumber = models.IntegerField(blank=False,null=False)
     accountType = models.ForeignKey(AccountType,on_delete=models.PROTECT, related_name ='accounttype',blank=True,null=True)
+    accountMemo = models.CharField(max_length=50,blank=False,null=False)
+
+    class Meta:
+        verbose_name_plural="BankAccount"
+        ordering = ('branchName',)
+
+    def __str__(self):
+        return self.name
+
+
+class Statement(models.Model):
 
 
     class Meta:
         verbose_name_plural="BankAccount"
-        ordering = ('name',)
+        ordering = ('branchName',)
 
     def __str__(self):
         return self.name
