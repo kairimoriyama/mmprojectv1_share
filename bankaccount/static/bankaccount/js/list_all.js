62a2253fdd481ab82b3d934fbf324ab572575de1
kairimoriyama <kairimoriyama@gmail.com>
2021-07-04 22:47:45 +0900

    update

---
+++ bankaccount/static/bankaccount/js/list_all.js
@@ -0,0 +1,50 @@
+
+// 検索条件をローカルストレージへ保存（ボタン：検索、移動、詳細、戻る）
+function set_search_key(){
+
+  localStorage.setItem('bankaccount_count_key', '1'); //カウント1を設定
+  localStorage.removeItem('bankaccount_search_key');
+
+  let selected_bankAccount = document.getElementById('selected_bankAccount').value;
+
+  let dataset = ({
+    "key1": selected_bankAccount,
+  });
+ 
+  let datasetJSON = JSON.stringify(dataset); // JSONに変換
+  localStorage.setItem('bankaccount_search_key', datasetJSON); 
+};
+
+
+function count_key_clear(){
+  localStorage.removeItem('bankaccount_count_key'); //他のページへ遷移する場合にcount_key削除
+};
+
+
+function get_search_key() {
+
+  let dataset = JSON.parse(localStorage.getItem('bankaccount_search_key'));
+
+  if (dataset === null) {
+
+  }else{
+
+    // 検索条件をフォームに入力
+    document.getElementById('selected_bankAccount').value= dataset["key1"];
+
+
+
+    // 更新・他のページからの遷移の際に検索実施
+    if (parseInt(localStorage.getItem('bankaccount_count_key'))==1) { //カウント1であればそのまま実行
+
+      localStorage.removeItem('bankaccount_count_key'); 
+      localStorage.removeItem('bankaccount_search_key');
+      
+    }else{
+      localStorage.setItem('bankaccount_count_key', '1'); //カウント1を設定
+      document.getElementById('search_btn').click(); //検索条件による抽出実行
+    };
+  };
+};
+
+window.onload = get_search_key(); //読み込み時に遷移前の検索条件を入力
